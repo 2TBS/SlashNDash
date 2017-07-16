46a473bdb3bd85ae46dc022b5a45db3ba20354eb
Vikram Peddinti <vikram.peddinti@gmail.com>
2017-07-15 20:29:55 -0700

    CameraShake/Player Setup/Damage

---
+++ Assets/Scripts/Pl_Manager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    Normal,
+    Hit,
+    Dead
+}
+
+public class Pl_Manager : MonoBehaviour {
+
+    //Basic Player Variables
+    private int health  = 100;
+    private State currState = State.Normal;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        //Check if dead
+		if(currState == State.Dead)
+        {
+            //do something related to being dead
+        }
+        else if(currState == State.Hit)
+        {
+            //Play the animation and vibrate and a lot more
+
+
+
+            //Reset back to normal state so we dont keep playing animations xD
+            currState = State.Normal;
+        }
+        else
+        {
+            //Nothing happened in between
+        }
+
+
+	}
+
+    /// <summary>
+    /// Applys a certain amount of damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void damageApply(int damage)
+    {
+        
+        health -= damage;
+        if(health <= 0)
+        {
+            currState = State.Dead;
+        }
+        else
+        {
+            currState = State.Hit;
+        }
+
+    }
+
+    public int getHealth()
+    {
+        return health;
+    }
+
+    public void resetPlayer()
+    {
+        //Resetting to default
+        health = 100;
+        currState = State.Normal;
+
+    }
+}
