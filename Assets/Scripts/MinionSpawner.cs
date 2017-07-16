e67e00c3cd52d18747090b6266e899e298be5879
Ben Cuan <ben.cuan@gmail.com>
2017-07-16 04:12:31 -0700

    Placeholder minion sprites

--- Assets/Scripts/MinionSpawner.cs
+++ Assets/Scripts/MinionSpawner.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class MinionSpawner : MonoBehaviour {
 
+	public Object[] cavePrefabs;
+	public List<GameObject> spawnedCaves;
+	public GameObject rock;
+
+	bool spawned;
+
+	public int SPAWN_DISTANCE = 15;
 	// Use this for initialization
 	void Start () {
-		
+		cavePrefabs = Resources.LoadAll("Caves/", typeof(GameObject));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		foreach(GameObject cave in spawnedCaves) {
+			if( Mathf.Abs(rock.transform.position.y - cave.transform.position.y) >= SPAWN_DISTANCE) {
+				
+				spawnedCaves.Remove(cave);
+				Destroy(cave);
+			}
+				
+		}
+
+		if((int)rock.transform.position.y % 5 == 0) {
+			if(!spawned)
+				spawnedCaves.Add(Instantiate((GameObject)cavePrefabs[Random.Range(0, cavePrefabs.Length-1)], rock.transform.position + Vector3.up*SPAWN_DISTANCE, Quaternion.Euler(-90,0,0)));
+			Debug.Log("Create cave");
+			spawned = true;
+		} else spawned = false;
 	}
 }
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+	Canvas ui;
+
+	// Use this for initialization
+	void Start () {
+		ui = GetComponent<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)) ui.enabled = false;
+
+	}
+
+}
