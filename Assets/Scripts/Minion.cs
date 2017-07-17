ed55bb2f2682be57ada26d2f1ebb7591667c774a
Ben Cuan <ben.cuan@gmail.com>
2017-07-17 12:11:48 -0700

    Fix runtime errors with minions

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -26,18 +26,22 @@ public class Minion : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         spawner = GameObject.FindObjectOfType<MinionSpawner>();
-        //Layer generation
 
+        //Layer generation
         for(int i = 0; i < spawner.GetDifficultyRange(); i++)
-        layers.Add(Instantiate((GameObject)spawner.minionPrefabs[Random.Range(0, spawner.minionPrefabs.Length)], transform.position, Quaternion.identity)
-            .GetComponent<MinionLayer>()
-            .Construct(i, gameObject));
+            layers.Add(Instantiate((GameObject)spawner.minionPrefabs[Random.Range(0, spawner.minionPrefabs.Length)], transform.position, Quaternion.identity)
+                .GetComponent<MinionLayer>()
+                .Construct(i, gameObject));
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        topLayer = layers[layers.Count - 1];
+        try {
+            topLayer = layers[layers.Count - 1];
+        }
+        catch {Debug.Log("No layers left");}
+       
         transform.Translate(Vector2.left / 10);
 	}
 }
