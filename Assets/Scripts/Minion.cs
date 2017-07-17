31a9af548eb2cf43ca6ab96e032020e08eeee1f3
Ben Cuan <ben.cuan@gmail.com>
2017-07-17 00:45:24 -0700

    Revamp minion and minion spawning code

--- Assets/Scripts/Minion.cs
+++ Assets/Scripts/Minion.cs
@@ -1,16 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Minion : MonoBehaviour {
 
+    public enum SwipeType : int
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        BottomLeftDiagonal,
+        TopLeftDiagonal,
+        Tap
+    }
+
+
+    List<MinionLayer> layers = new List<MinionLayer>();
+
+    MinionLayer topLayer;
+
+    MinionSpawner spawner;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        spawner = GameObject.FindObjectOfType<MinionSpawner>();
+        //Layer generation
+
+        for(int i = 0; i < spawner.GetDifficultyRange(); i++)
+        layers.Add(Instantiate((GameObject)spawner.minionPrefabs[Random.Range(0, spawner.minionPrefabs.Length)], transform.position, Quaternion.identity)
+            .GetComponent<MinionLayer>()
+            .Construct(i, gameObject));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        topLayer = layers[layers.Count - 1];
         transform.Translate(Vector2.left / 10);
 	}
 }
---
+++ Assets/Scripts/MinionLayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Attached to each layer Prefab. Instantiated by the Minion object.
+/// </summary>
+public class MinionLayer : MonoBehaviour {
+
+    const float SIZE_MULT = 1.5f;
+
+    public Minion.SwipeType swipe; //to be assigned in inspector
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Makeshift constructor. Run after instantiation.
+    /// </summary>
+    public MinionLayer Construct(int size, GameObject parent)
+    {
+        transform.SetParent(parent.transform);
+        transform.localScale = Vector2.one * (size+1) * SIZE_MULT;
+
+        return this;
+     
+    }
+}
