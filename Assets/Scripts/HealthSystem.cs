cc9af7a70ea254eb63904abc34842e5cfedce71f
Omar Hossain <omarepic1@gmail.com>
2017-07-19 18:42:16 -0700

    HealthBar Bug Fix 1

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -14,11 +14,14 @@ public class HealthSystem : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		DealDamage (.01f);
-		print (transform.localScale.x);
+		if (transform.localScale.x > 0) {
+			DealDamage (2f);
+			//print (transform.localScale.x);
+		}
 	}
 
 	void DealDamage(float percentDamage) {
-		transform.localScale = new Vector3( originalSize - (originalSize * (percentDamage / 100)), transform.localScale.y, transform.localScale.z);
+		transform.localScale -= new Vector3( originalSize * (percentDamage / 100), 0, 0);
+		transform.position -= new Vector3( originalSize * (percentDamage / 100) * 3.2f, 0, 0);
 	}
 }
